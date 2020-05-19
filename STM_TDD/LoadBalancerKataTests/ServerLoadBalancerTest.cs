8d76dc0822bfce9f9ddb860dceee5ccefeefae65
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-20 01:47:50 +0200

    Added NHamcrest package, added test ServerShouldStayEmptyIfHadNoVMsDuringBalancing that fails (#1red)

--- STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
+++ STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using NHamcrest;
+using Xunit;
+using Assert = NHamcrest.XUnit.Assert;
+
 namespace LoadBalancerKataTests
 {
 	public class ServerLoadBalancerTest
@@ -6,7 +9,16 @@ namespace LoadBalancerKataTests
 		[Fact]
 		public void ItCompiles()
 		{
-			Assert.True(true);
+			Assert.That(true, Is.True());
+		}
+
+		[Fact]
+		public void ServerShouldStayEmptyIfHadNoVMsDuringBalancing()
+		{
+			Server server = A(Server().WithCapacity(1));
+
+			Balance(AListOfServersWith(server), AnEmptyListOfVMs());
+			Assert.That(server, HasLoadPercentageOf(0.0d));
 		}
 	}
 }
