c36cef1925322f9a80cd7a04ce74d309f3902b7b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-30 12:29:50 +0200

    Added test case for multiple vms applied to one server with specified capacity.

--- STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
+++ STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
@@ -6,6 +6,7 @@ using Assert = NHamcrest.XUnit.Assert;
 using static LoadBalancerKataTests.CurrentLoadPercentageMatcher;
 using static LoadBalancerKataTests.ServerBuilder;
 using static LoadBalancerKataTests.VmBuilder;
+using static NHamcrest.Is;
 namespace LoadBalancerKataTests
 {
 	public class ServerLoadBalancerTest
@@ -43,12 +44,23 @@ namespace LoadBalancerKataTests
 		[Fact]
 		public void ServerWithLargeCapacityShouldContainAllProvidedVMsAfterBalancingAndHaveSpecifiedLoadPercentageRate()
 		{
-			
+			var server = A(Server().WithCapacity(20));
+			var (vm1, vm2, vm3, vm4, vm5) = (A(Vm().WithSize(2)), A(Vm().WithSize(3)), A(Vm().WithSize(1)),
+				A(Vm().WithSize(5)), A(Vm().WithSize(4)));
+			Balance(AListOfServersWith(server), AListOfVms(vm1, vm2, vm3, vm4, vm5));
+			var vms = server.Vms;
+			Assert.That(vms, Has.Item(EqualTo(vm1)));
+			Assert.That(vms, Has.Item(EqualTo(vm2)));
+			Assert.That(vms, Has.Item(EqualTo(vm3)));
+			Assert.That(vms, Has.Item(EqualTo(vm4)));
+			Assert.That(vms, Has.Item(EqualTo(vm5)));
+			Assert.That(server, HasLoadPercentageOf(75.0));
 		}
 
 		private static ICollection<Vm> AListOfVms(params Vm[] vm) => vm;
 
-		private static void Balance(ICollection<Server> servers, ICollection<Vm> vms) => new ServerLoadBalancer().Balance(servers, vms);
+		private static void Balance(ICollection<Server> servers, ICollection<Vm> vms) =>
+			new ServerLoadBalancer().Balance(servers, vms);
 
 		private static ICollection<Server> AListOfServersWith(params Server[] values) => values;
 
