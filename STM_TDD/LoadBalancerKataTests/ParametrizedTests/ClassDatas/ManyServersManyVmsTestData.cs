f32988dfd1ec21632693f9dd84bbe9f2bf222243
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-03 03:38:58 +0200

    Added base of ManyServersManyVmsTestData, added parametrized test for balancing many servers and many vms case.

---
+++ STM_TDD/LoadBalancerKataTests/ParametrizedTests/ClassDatas/ManyServersManyVmsTestData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using static LoadBalancerKataTests.Builders.ServerBuilder;
+using static LoadBalancerKataTests.Builders.VmBuilder;
+using static LoadBalancerKataTests.ServerLoadBalancerTestBase;
+
+namespace LoadBalancerKataTests.ParametrizedTests.ClassDatas
+{
+   public class ManyServersManyVmsTestData : IEnumerable<object[]>
+   {
+	   public IEnumerator<object[]> GetEnumerator()
+	   {
+		   throw new System.NotImplementedException();
+	   }
+
+	   IEnumerator IEnumerable.GetEnumerator()
+	   {
+		   return GetEnumerator();
+	   }
+   }
+}
