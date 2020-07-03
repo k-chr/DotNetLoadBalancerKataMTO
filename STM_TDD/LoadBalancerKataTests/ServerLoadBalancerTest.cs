b5d8c2aefc60d7cffdb55df9361115a15945e400
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-03 15:12:53 +0200

    Added ServerVmMatcher, Added Red Test

--- STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
+++ STM_TDD/LoadBalancerKataTests/ServerLoadBalancerTest.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using LoadBalancerKata;
 using NHamcrest;
 using Xunit;
@@ -40,6 +42,18 @@ namespace LoadBalancerKataTests
 			Assert.That(server, HasLoadPercentageOf(50.0));
 		}
 
+		[Fact]
+		public void ServerShouldFillSomeVmsAfterBalancing()
+		{
+			var server = A(Server().WithCapacity(20));
+			var (vm1, vm2, vm3) = (A(Vm().WithSize(2)), A(Vm().WithSize(3)), A(Vm().WithSize(7)));
+			Balance(AListOfServersWith(server), AListOfVms(vm1, vm2, vm3));
+			Assert.That(server, HasLoadPercentageOf(60.0));
+			Assert.That(server, HasVmsEqualTo(AListOfVms(vm1, vm2, vm3)));
+		}
+
+		private IMatcher<Server> HasVmsEqualTo(ICollection<Vm> aListOfVms) => new ServerVmMatcher(aListOfVms);
+
 		private static ICollection<Vm> AListOfVms(params Vm[] vm) => vm;
 
 		private static void Balance(ICollection<Server> servers, ICollection<Vm> vms) => new ServerLoadBalancer().Balance(servers, vms);
@@ -49,5 +63,27 @@ namespace LoadBalancerKataTests
 		private static ICollection<Vm> AnEmptyListOfVMs() => new List<Vm>();
 
 		private static T A<T>(IBuilder<T> builder) => builder.Build();
+	}
+
+	internal class ServerVmMatcher : IMatcher<Server>
+	{
+		private readonly ICollection<Vm> _aListOfVms;
+
+		public ServerVmMatcher(ICollection<Vm> aListOfVms)
+		{
+			_aListOfVms = aListOfVms;
+		}
+
+		public void DescribeTo(IDescription description)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool Matches(Server item) => _aListOfVms.SequenceEqual(item?.Vms ?? new List<Vm>());
+
+		public void DescribeMismatch(Server item, IDescription mismatchDescription)
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
