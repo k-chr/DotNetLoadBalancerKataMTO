7e3d68de18b44d128eb03cfe93b6ddeb1ec56de3
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-07-03 02:40:12 +0200

    Added basic test case for ServerLoadBalanceParametrizedTests

--- STM_TDD/LoadBalancerKataTests/ParametrizedTests/ServerLoadBalanceParametrizedTests.cs
+++ STM_TDD/LoadBalancerKataTests/ParametrizedTests/ServerLoadBalanceParametrizedTests.cs
@@ -1,6 +1,23 @@
-﻿namespace LoadBalancerKataTests.ParametrizedTests
+﻿using NHamcrest;
+using static LoadBalancerKataTests.Builders.ServerBuilder;
+using static LoadBalancerKataTests.Builders.VmBuilder;
+using static LoadBalancerKataTests.Matchers.CurrentLoadPercentageMatcher;
+using static NHamcrest.Is;
+using Xunit;
+using Assert = NHamcrest.XUnit.Assert;
+
+namespace LoadBalancerKataTests.ParametrizedTests
 {
-   public class ServerLoadBalanceParametrizedTests
+   public class ServerLoadBalanceParametrizedTests : ServerLoadBalancerTestBase
    {
+	   [Fact]
+	   public void BalancingOneServerWithOneSlotCapacityAndOneSlotVmFillsTheServerWithTheVm()
+	   {
+		   var server = A(Server().WithCapacity(1));
+		   var vm = A(Vm().WithSize(1));
+		   Balance(AListOfServersWith(server), AListOfVms(vm));
+		   Assert.That(server, HasLoadPercentageOf(100.0));
+		   Assert.That(server.Vms, Has.Item(EqualTo(vm)));
+	   }
    }
 }
